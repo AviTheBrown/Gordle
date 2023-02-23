a17192e7ce42ccce5b843caa7fb4a1f85c0d265c
Avi Brown <avinbrown.focus@icloud.com>
2023-02-22 23:58:30 -0500

    Added ask method for Game instance.

--- gordle/gameEngine.go
+++ gordle/gameEngine.go
@@ -1,14 +1,37 @@
 package  gordle
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+)
 
-type Game struct {}
+type Game struct {
+    reader *bufio.Reader
+}
 
 // New -> creates a new intance of Game.
     //Entry point.
-func New() *Game {
-    g := &Game{}
+func New(playerInfo io.Reader) *Game {
+    g := &Game{
+        reader: bufio.NewReader(playerInfo),
+    }
     return g
+}
+
+const wordlength = 5
+func (g *Game) ask() []rune  {
+    fmt.Printf("Enter a %d-character guess:\n", wordlength)
+
+    for  {
+        playerInput, _, err := g.reader.ReadLine()
+        if err != nil {
+            _, _ = fmt.Fprintf(os.Stdout, "Gordle failed to read your guess: %s\n, ",
+                err.Error())
+        }
+        guess :=[]rune(string(playerInput))
+    }
 }
 
 func (g *Game) Play() {
